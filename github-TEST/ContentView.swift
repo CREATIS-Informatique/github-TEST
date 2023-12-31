0de7674cb8fb0e9418b7332e521e24c51867f49d
Rosat Pierre-Yves <rosatp@me.com>
2023-12-31 09:12:56 +0100

    Initial Commit

---
+++ github-TEST/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  github-TEST
+//
+//  Created by Pierre-Yves Rosat on 31.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, you")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
